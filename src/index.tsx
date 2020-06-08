32d46b13fb01bd30a665995a43b37c6a321954eb
Pon Cheol Ku <bbon@bbon.kr>
2020-06-08 21:32:08 +0900

    기능:서비스워커 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,24 @@ import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import JsonFomatter from './components/JsonFomatter';
-import OfflinePluginRuntime from 'offline-plugin/runtime';
+// import OfflinePluginRuntime from 'offline-plugin/runtime';
 
-OfflinePluginRuntime.install();
+// OfflinePluginRuntime.install();
 
 const Hot = hot(JsonFomatter);
 
 ReactDOM.render(<Hot />, document.querySelector('#app'));
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then((registration) => {
+                // tslint:disable:no-console
+                console.log('SW registered: ', registration);
+            })
+            .catch((registrationError) => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
+}
