8143f2df7bd3a92bc32fa20e27774ce7156deefa
Pon Cheol Ku <bbon@bbon.kr>
2020-06-08 17:42:51 +0900

    리팩터링:타입스크립트
    - 타입스크립트를 사용합니다.
    - eslint를 적용
    - prettier 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,57 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  name: "json-fomatter-build",
-  mode: isProduction ? "production" : "dev",
-  devtool: isProduction ? "hidden-source-map" : "eval",
-  resolve: {
-    extensions: [".jsx", ".js"],
-  },
-  entry: {
-    app: "./src/jsx/index",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
-        exclude: path.join(__dirname, "node_modules"),
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-      },
+    name: 'json-fomatter-build',
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'hidden-source-map' : 'eval',
+    resolve: {
+        extensions: ['.jsx', '.js', '.ts', '.tsx'],
+    },
+    entry: {
+        app: './src/index',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                },
+                exclude: path.join(__dirname, 'node_modules'),
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: !isProduction }),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'public',
+                    to: '',
+                },
+            ],
+        }),
     ],
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({ debug: !isProduction }),
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "public",
-          to: "",
-        },
-      ],
-    }),
-  ],
-  output: {
-    filename: "dist/[name].js",
-    path: path.join(__dirname, ".build"),
-    // publicPath: "",
-  },
+    output: {
+        filename: 'dist/[name].js',
+        path: path.join(__dirname, '.build'),
+    },
 };
